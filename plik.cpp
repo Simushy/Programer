739280d5f7817c4f2fcb70d9aaa39d37007ba178
Simushy <simushy@gmail.com>
2023-10-13 07:58:24 +0000

    Utworzenie pliku z zadaniami

---
+++ plik.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+Zadania:
+* Napisz funkcję, która dla kolekcji danych liczbowych obliczy średnią arytmetyczną.
+* Napisz funkcję, która dla kolekcji danych liczbowych policzy ile jest liczb większych od średniej arytmetycznej.
+* Napisz funkcje, która dla kolekcji danych liczbowych przeniesie te liczby do innej kolekcji w odwrotnej kolejności.
+* Napisz funkcje, która dla kolekcji danych liczbowych obliczy częstotliwość występowania danej liczby.
+* Napisz funkcję, która dla kolekcji danych liczbowych znajdzie najdłuższy rosnący podciąg.
+* Napisz funkcję, która dla kolekcji danych liczbowych przeniesie te liczby do osobnych kolekcji liczb parzystych i nieparzystych.
+*/
+
+int main()
+{
+}
